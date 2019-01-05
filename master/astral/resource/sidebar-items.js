ab8c88c44b05bd1672a3fb4871fd230bb39d7dbe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-05 23:59:13 +0000

    Deploy astral-engine/docs to github.com/astral-engine/docs.git:master

---
+++ master/astral/resource/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ErrorKind","A list specifying general categories of resource error."]],"mod":[["assets","Functionality to manage asset data."]],"struct":[["Declaration",""],["System",""]],"trait":[["LoadData",""],["Resource",""]],"type":[["Error",""],["LoadPriority",""],["Result",""]]});
