7edbfdffa7a03e54af43c224cc705b197c2f4bb0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-24 23:54:03 +0000

    Deploy astral-engine/docs to github.com/astral-engine/docs.git:master

---
+++ master/astral/third_party/rayon/prelude/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["ParallelSlice","Parallel extensions for slices."],["ParallelSliceMut","Parallel extensions for mutable slices."],["ParallelString","Parallel extensions for strings."]]});
