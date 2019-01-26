8ff9a953de726f56b314256cd90dcc87d94b5583
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-26 23:53:37 +0000

    Deploy astral-engine/docs to github.com/astral-engine/docs.git:master

---
+++ master/astral_engine/util/hash/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Murmur3","An implementation of the [Murmur3 Hash]."],["NopHasher","An implementation of [`Hasher`] hasher which only accepts values with a size of 8 bytes or an integral value fitting into 8 bytes."]]});
---
+++ master/implementors/astral/third_party/num_traits/ops/checked/trait.CheckedShr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["astral"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
